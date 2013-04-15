bb093ad7b0808ed23fef6df84b9ed42fbf4067b2
Jonathan Gaull <Jonbeesh@gmail.com>
2013-04-15 00:14:35 -0700

    Got FastFacebook working mostly from FastTweet codes.

--- FastFacebook/AppDelegate.m
+++ FastFacebook/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
@@ -36,6 +37,9 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    ViewController *viewController = (ViewController *)self.window.rootViewController;
+    [viewController regainFocus];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- FastFacebook/ViewController.m
+++ FastFacebook/ViewController.m
@@ -1,12 +1,13 @@
 //
 //  ViewController.m
-//  FastFacebook
+//  FastTweet
 //
 //  Created by Jonathan Gaull on 4/14/13.
-//  Copyright (c) 2013 OneHeaded Llama. All rights reserved.
+//  Copyright (c) 2013 OneHeadedLlama. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
 
 @interface ViewController ()
 
@@ -20,10 +21,29 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self showTweetView];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)showTweetView {
+    SLComposeViewController *tweetSheet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+    tweetSheet.completionHandler = ^(SLComposeViewControllerResult result) {
+        [self showTweetView];
+    };
+    
+    [self presentViewController:tweetSheet animated:YES completion:nil];
+}
+
+- (void)regainFocus {
+    [self showTweetView];
 }
 
 @end
